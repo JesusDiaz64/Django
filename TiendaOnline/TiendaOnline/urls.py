61652f2b824258edf54b8b3a926e01c965d0bbaa
JesusDiaz64 <jesusdaviddiazcabrales@gmail.com>
2025-01-13 11:09:45 -0800

    Muchos cambios

--- TiendaOnline/TiendaOnline/urls.py
+++ TiendaOnline/TiendaOnline/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gestionPedidos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda_productos/', views.busqueda_productos),
+    path('buscar/', views.buscar),
 ]
 
 #............ ESTE SE UTILIZA PARA ABRIR LAS PLANTILLAS CREADAS.
--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -3,9 +3,25 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "telefono") 
+    # SE CREA UNA LISTA A LA CUAL SE MOSTRARA EN PANTALLA DEL ADMINISTRADOR.
+    # LLAMAR LA CLASE A IMPRIMIR EN REGISTER PARA VISUALIZAR.
+    
+    search_fields = ("nombre", "telefono")
+    #BUSQUEDA
+    
+class ArticulosAdmin(admin.ModelAdmin): # Filtro para productos dentro el panel de administracion
+    list_filter = ("seccion",) # Esto es una tupla
+    
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha")
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha" # Pequeña pestaña de busqueda
+
+admin.site.register(Clientes, ClientesAdmin) # AQUI.
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
 
 #....... ES UN REGISTRÓ PARA POSTGRESQL PARA QUE LO UTILICE EL ADMINISTRADOR.
 #....... CREACIÓN DE TABLAS EN LA PAGINA DEL ADMINISTRADOR.
